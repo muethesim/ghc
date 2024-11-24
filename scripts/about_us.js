5aa83de2ee921c1c1696ff0121dfce9ce0c437cd
muethesim <muethesim@gmail.com>
2024-11-24 11:08:16 +0530

    Phase 1 completed

--- scripts/about_us.js
+++ scripts/about_us.js
@@ -18,3 +18,56 @@ hamburger.addEventListener("click", () => {
     }, 300);
   }
 });
+
+const expertise_list_item = document.querySelector(".expertise-list-item");
+const drop_down = document.querySelector(".drop-down");
+const expertise_drop_down = document.querySelector(".expertise-drop-down");
+const main = document.querySelector(".main")
+var list_out = true;
+var container_out = true;
+
+expertise_list_item.addEventListener("mouseover", () => {
+  drop_down.classList.add("active");
+  expertise_drop_down.classList.add("active");
+  list_out = false;
+});
+
+expertise_list_item.addEventListener("mouseout", () => {
+  if (container_out && list_out) {
+    drop_down.classList.remove("active");
+    expertise_drop_down.classList.remove("active");
+  }
+});
+
+expertise_list_item.addEventListener("mouseout", () => {
+  setTimeout(() => {
+    list_out = true;
+  }, 200);
+});
+
+expertise_drop_down.addEventListener("mouseover", () => {
+  drop_down.classList.add("active");
+  expertise_drop_down.classList.add("active");
+  container_out = false;
+});
+
+main.addEventListener("mouseover", () => {
+  if (container_out && list_out) {
+    drop_down.classList.remove("active");
+    expertise_drop_down.classList.remove("active");
+  }
+});
+
+expertise_drop_down.addEventListener("mouseout", () => {
+  container_out = true;
+  if (container_out && list_out) {
+    drop_down.classList.remove("active");
+    expertise_drop_down.classList.remove("active");
+  }
+});
+
+expertise_drop_down.addEventListener("mouseout", () => {
+  setTimeout(() => {
+    container_out = true;
+  }, 200);
+});
