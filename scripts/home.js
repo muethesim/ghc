9b5725ca788deba7753e19db6ff48ad505436e74
muethesim <muethesim@gmail.com>
2024-11-28 13:52:21 +0530

    expertise completed

--- scripts/home.js
+++ scripts/home.js
@@ -33,20 +33,33 @@ const hamburger = document.querySelector(".hamburger");
 const social_media_menu = document.getElementById("social-media-menu");
 const social_media_links = document.getElementById("social-media-links");
 const change_language = document.querySelector("change-language");
+const navbar = document.querySelector(".navbar")
 
 hamburger.addEventListener("click", () => {
-  if (social_media_menu.classList.contains("activate")) {
-    social_media_menu.classList.remove("activate");
-    hamburger.classList.remove("activate");
-    social_media_links.classList.remove("activate");
-    change_language.classList.remove("activate");
+  if (screen.width > 1025) {
+    if (social_media_menu.classList.contains("activate")) {
+      social_media_menu.classList.remove("activate");
+      hamburger.classList.remove("activate");
+      social_media_links.classList.remove("activate");
+      change_language.classList.remove("activate");
+    } else {
+      social_media_menu.classList.add("activate");
+      hamburger.classList.add("activate");
+      setTimeout(() => {
+        social_media_links.classList.add("activate");
+        change_language.classList.add("activate");
+      }, 300);
+    }
   } else {
-    social_media_menu.classList.add("activate");
-    hamburger.classList.add("activate");
-    setTimeout(() => {
-      social_media_links.classList.add("activate");
-      change_language.classList.add("activate");
-    }, 300);
+    if (navbar.classList.contains("activate")) {
+      navbar.classList.remove("activate");
+      hamburger.classList.remove("activate");
+      document.body.style.overflow = "";
+    } else {
+      navbar.classList.add("activate");
+      hamburger.classList.add("activate");
+      document.body.style.overflow = "hidden";
+    }
   }
 });
 
