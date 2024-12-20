7b6b5178198b609cd50d9522a1f629ba0a346e93
muethesim <muethesim@gmail.com>
2024-12-20 22:39:31 +0530

    Drag

--- scripts/about_us.js
+++ scripts/about_us.js
@@ -685,3 +685,44 @@ document.querySelector("#awards").addEventListener("mouseover", () => {
 document.querySelector("#events").addEventListener("mouseover", () => {
   document.querySelector(".media-room-drop-down .drop-down-description > a").href = "/event_list.html"
 })
+
+
+function clickAndDrag(selector, scroll_speed = 3, classOnEvent = 'grabbed_elem') {
+  const slider = document.querySelector(selector);
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      isDown = true;
+      slider.classList.add(classOnEvent);
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+  
+      // prevent default child behavior
+      document.body.addEventListener('click', function( event ){
+          if (slider.contains(event.target)) {
+              event.preventDefault();
+          }
+      });
+  });
+  slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove(classOnEvent);
+  });
+  slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove(classOnEvent);
+  });
+  slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * scroll_speed; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+  });
+}
+
+clickAndDrag(".history-corousal")
+clickAndDrag(".number-corousal")
