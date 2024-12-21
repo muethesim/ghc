52a1d7b54411de0980ccb06abfc8e5e4a152008b
Muethesim <muethesim@gmail.com>
2024-12-21 06:11:48 +0530

    Single corousal in every page

--- scripts/about_us.js
+++ scripts/about_us.js
@@ -1,25 +1,25 @@
 // Corousal
 
-var options = {
-  accessibility: true,
-  prevNextButtons: true,
-  pageDots: true,
-  wrapAround: true,
-  setGallerySize: false,
-  autoPlay: 3000,
-  arrowShape: {
-    x0: 10,
-    x1: 60,
-    y1: 50,
-    x2: 60,
-    y2: 45,
-    x3: 15,
-  },
-};
+// var options = {
+//   accessibility: true,
+//   prevNextButtons: true,
+//   pageDots: true,
+//   wrapAround: true,
+//   setGallerySize: false,
+//   autoPlay: 3000,
+//   arrowShape: {
+//     x0: 10,
+//     x1: 60,
+//     y1: 50,
+//     x2: 60,
+//     y2: 45,
+//     x3: 15,
+//   },
+// };
 
-var carousel = document.querySelector("[data-carousel]");
-var slides = document.getElementsByClassName("carousel-cell");
-var flkty = new Flickity(carousel, options);
+// var carousel = document.querySelector("[data-carousel]");
+// var slides = document.getElementsByClassName("carousel-cell");
+// var flkty = new Flickity(carousel, options);
 
  var headerContainerWidthForDropDown =
   document.querySelector(".header-container").offsetWidth;
